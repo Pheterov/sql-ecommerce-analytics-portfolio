aefd8b0735d14b4b652e6f578824c552382d77d9
Piotr <110790920+Pheterov@users.noreply.github.com>
2026-03-18 19:52:32 +0100

    Update 2_analytics_portfolio_advanced_concepts.sql
    tons of work with query #3

--- 2_analytics_portfolio_advanced_concepts.sql
+++ 2_analytics_portfolio_advanced_concepts.sql
@@ -250,36 +250,136 @@ FROM customer_orders
 ORDER BY customer_id, order_date, order_id;
 
 /*===================================================================================================
-4️⃣ Month-over-Month AOV Change
-🎯 Goal: Measure average order value trends
+4️⃣ Year-over-Year revenue change
+🎯 Goal: Measure revenue trends
 🛠️ Stack: SQL
-💡 Impact: Identifies pricing strategy effectiveness
+💡 Impact: Serves as a starting point for diagnosing drivers
 ====================================================================================================*/
-WITH monthly_metrics AS
+WITH yearly_metrics AS
 (
 SELECT
-	DATE_FORMAT(o.order_date, '%Y-%m-01')						 													month
+	EXTRACT(YEAR FROM o.order_date)						 															year
 	,COUNT(DISTINCT op.order_id) 																					orders_cnt
-	,SUM(op.item_quantity*COALESCE(p.product_price,0)*(1-COALESCE(op.position_discount,0))) 						revenue
+	,SUM(op.item_quantity*COALESCE(p.product_price,0)*
+		(1-COALESCE(op.position_discount,0))) 																		revenue
 FROM orders o
 JOIN order_positions op ON o.order_id = op.order_id
 JOIN products p ON op.product_id = p.product_id
-GROUP BY month
-), monthly_averages AS
-(
-SELECT
-month
-	,revenue
-	,revenue / orders_cnt 																							AoV
-FROM monthly_metrics
+GROUP BY year
 )
 SELECT
-	month
+	year
+	,ROUND(revenue, 2)																								revenue
 	,ROUND(
-		(AoV - LAG(AoV) OVER (ORDER BY month)) * 100.0 /
-		NULLIF(LAG(AoV) OVER (ORDER BY month),0), 2) AoV_pct_change_vs_previous_month
-FROM monthly_averages
-ORDER BY month;
+		(revenue - LAG(revenue) OVER (ORDER BY year)) * 100.0 /
+		NULLIF(LAG(revenue) OVER (ORDER BY year),0), 2) 															revenue_pct_change_vs_previous_year
+FROM yearly_metrics
+ORDER BY year;
+
+/*==============================================================================================================
+🎯 Goal: Show difference between baseline metric vs enhanced insight.
+		  Evaluate city-level performance by comparing year-over-year changes in revenue, customer base, and order value.
+		  Identify whether growth is driven by customer expansion, higher spending per order, or both.
+
+🛠️ Stack: SQL
+
+💡 Business Impact:
+Enables more precise allocation of marketing and expansion budgets by distinguishing
+between cities growing through customer acquisition versus higher monetization.
+Highlights markets where revenue growth may be unsustainable
+due to declining order value or shrinking customer base.Supports strategic decisions
+on whether to prioritize scaling demand or improving revenue quality in specific regions.
+
+🔍 Key Insights:
+Strong growth markets (e.g. New York City, Los Angeles) are driven
+by both increasing customer base and rising average order value, indicating healthy and scalable expansion.
+Some cities (e.g. San Diego) show rapid growth primarily fueled by higher order value, 
+suggesting potential pricing or basket-size optimization effects.Declining markets (e.g. San Francisco)
+exhibit simultaneous drops in revenue, customers, and AOV, signaling structural demand issues rather than temporary fluctuations.
+Mixed-signal cities (e.g. Philadelphia) reveal customer growth alongside decreasing AOV,
+which may indicate reliance on discounts or lower-value transactions to drive volume.
+
+📊 Example KPI:
+| revenue_share_pct | delivery_city  | revenue_2019 | revenue_2018 | revenue_growth_pct | unique_customers_2019 | unique_customers_2018 | customers_diff | avg_order_value_2019 | avg_order_value_2018 | avg_order_value_diff | avg_order_value_pct_diff |
+|-------------------|----------------|--------------|--------------|--------------------|-----------------------|-----------------------|----------------|----------------------|----------------------|----------------------|--------------------------|
+|             15,83 | New York City  |    74 849,79 |    35 206,02 |          	 112.61 |                    98 |                    67 |             31 |               741.09 |               525,46 |      	       215,63 |               	   29,10 |
+|              8,32 | Los Angeles    |    39 355,24 |    21 941,66 |           	  79.36 |                    74 |                    58 |             16 |               491.94 |               371,89 |               120,05 |                	   24,40 |
+|              4,53 | Seattle        |    21 427,00 |    22 847,99 |           	  -6.22 |                    38 |                    35 |              3 |               549.41 |               634,67 |        	   -85,26 |                	  -15,52 |
+|              4,09 | Philadelphia   |    19 322,31 |    14 663,35 |           	  31.77 |                    60 |                    42 |             18 |               316.76 |               349,13 |           	   -32,37 |               	  -10,22 |
+|              3,35 | San Francisco  |    15 855,28 |    28 143,73 |          	 -43.66 |                    37 |                    53 |            -16 |               406.55 |               521,18 |       	   	  -114,63 |                   -28,20 |
+
+===============================================================================================================*/
+
+WITH local_sales AS
+(
+SELECT
+	o.delivery_city                                																	delivery_city
+	,SUM(CASE 
+			WHEN EXTRACT(YEAR FROM o.order_date) = 2019 
+			THEN op.item_quantity*COALESCE(p.product_price, 0)*
+				(1-COALESCE(op.position_discount, 0))
+			ELSE 0
+			END) 																									revenue_2019
+	,SUM(CASE 
+			WHEN EXTRACT(YEAR FROM o.order_date) = 2018 
+			THEN op.item_quantity*COALESCE(p.product_price, 0)*
+				(1-COALESCE(op.position_discount, 0))
+			ELSE 0
+			END) 										 															revenue_2018
+	,COUNT(DISTINCT CASE
+		WHEN EXTRACT(YEAR FROM o.order_date) = 2019 
+		THEN o.order_id
+		END) 																										orders_cnt_2019
+	,ROUND(SUM(CASE
+		WHEN EXTRACT(YEAR FROM o.order_date) = 2019 
+		THEN op.item_quantity*COALESCE(p.product_price, 0)*
+			(1-COALESCE(op.position_discount,0))
+		ELSE 0
+		END) * 1.0 / 
+		NULLIF(
+			COUNT(DISTINCT CASE
+					WHEN EXTRACT(YEAR FROM o.order_date) = 2019 
+					THEN o.order_id END),0), 2) 																	avg_order_value_2019
+	,ROUND(SUM(CASE
+		WHEN EXTRACT(YEAR FROM o.order_date) = 2018 
+		THEN op.item_quantity*COALESCE(p.product_price, 0)*
+			(1-COALESCE(op.position_discount,0))
+		ELSE 0
+		END) * 1.0 / 
+		NULLIF(
+			COUNT(DISTINCT CASE
+					WHEN EXTRACT(YEAR FROM o.order_date) = 2018 
+					THEN o.order_id END),0), 2) 																	avg_order_value_2018
+	,COUNT(DISTINCT CASE
+			WHEN EXTRACT(YEAR FROM o.order_date) = 2019 
+			THEN o.customer_id END) 																				unique_customers_2019
+	,COUNT(DISTINCT CASE
+			WHEN EXTRACT(YEAR FROM o.order_date) = 2018 
+			THEN o.customer_id END) 																				unique_customers_2018
+FROM order_positions op
+JOIN orders o ON op.order_id = o.order_id
+JOIN products p ON op.product_id = p.product_id
+WHERE EXTRACT(YEAR FROM o.order_date) IN (2018, 2019)
+GROUP BY o.delivery_city
+)
+SELECT
+	ROUND(revenue_2019 * 100.0 /
+		SUM(revenue_2019) OVER (), 2) 																				revenue_share_pct
+	,delivery_city
+	,ROUND(revenue_2019, 2)																							revenue_2019
+	,ROUND(revenue_2018, 2)																							revenue_2018
+	,ROUND((revenue_2019 - revenue_2018) * 100.0 / 
+		NULLIF(revenue_2018,0), 2) 																					revenue_growth_pct	
+	,unique_customers_2019
+	,unique_customers_2018
+	,unique_customers_2019 - unique_customers_2018																	customers_diff
+	,avg_order_value_2019
+	,avg_order_value_2018
+	,avg_order_value_2019 - avg_order_value_2018																	avg_order_value_diff
+	,ROUND((avg_order_value_2019 - avg_order_value_2018) /
+		avg_order_value_2019*100.0, 2)																				avg_order_value_pct_diff
+FROM local_sales
+ORDER BY revenue_2019 DESC;
 
 /*===================================================================================================
 5️⃣ Top 2 Products by Revenue within Each Category
