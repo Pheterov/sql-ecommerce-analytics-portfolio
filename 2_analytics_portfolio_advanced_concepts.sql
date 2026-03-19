43f189ed3c3bd9ceb63412d14ac7e16a06d78720
Piotr <110790920+Pheterov@users.noreply.github.com>
2026-03-19 13:06:18 +0100

    Update 2_analytics_portfolio_advanced_concepts.sql

--- 2_analytics_portfolio_advanced_concepts.sql
+++ 2_analytics_portfolio_advanced_concepts.sql
@@ -7,19 +7,8 @@
 
 /*================================================================================================================================================================================================
 1️⃣ Revenue and Order Count by Delivery State
-
-This basic metric doesn't really tell us anything... Can we make an informed and profitable decision based on a report like this? 
-We’ve only identified which region is the most profitable, but let’s dig a little deeper and try to figure out why, step by step.
-What's next then ? Maybe it'd be nice to see performance over time.
-
-Query result snippet:
-
-| delivery_state | revenue    | orders_cnt  |
-|----------------|------------|-------------|
-| California 	 | 451 450,55 | 	  1 021 |
-| New York		 | 312 376,98 | 		562 |
-| Texas			 | 164 948,68 | 		487 |
-===============================================================================================================================================================================================*/
+================================================================================================================================================================================================*/
+	   
 SELECT
 	o.delivery_state
 	,ROUND(SUM(op.item_quantity*COALESCE(p.product_price,0)*(1-COALESCE(op.position_discount,0))), 2) 				revenue
@@ -30,24 +19,23 @@ JOIN products p ON op.product_id = p.product_id
 GROUP BY o.delivery_state
 ORDER BY revenue DESC;
 
-/*================================================================================================================================================================================================
-2️⃣ YoY performance
-
-Result is suspicious... immediately raises a red flag.
-Between 2018 - 2021 California was doing fantastic and then in 2022... sudden ~90% revenue drop.
-Such a drastic change is highly unlikely from a business perspective.
-
+/*================================================================================================================================================================================================	   
 Query result snippet:
 
-| year | delivery_state | revenue    | orders_cnt |
-|------|----------------|------------|------------|
-| 2022 | California     |  16 186,48 |         37 |
-| 2021 | California     | 148 729,44 |        336 |
-| 2020 | California     | 121 925,07 |        279 |
-| 2019 | California     |  93 307,09 |        198 |
-| 2018 | California     |  71 302,47 |        171 |
+| delivery_state | revenue    | orders_cnt  |
+|----------------|------------|-------------|
+| California 	 | 451 450,55 | 	  1 021 |
+| New York		 | 312 376,98 | 		562 |
+| Texas			 | 164 948,68 | 		487 |
 
-This query is a classic example of how misleading conclusions can arise from “just take the average” type of thinking.
+This basic metric doesn't really tell us anything... Can we make an informed and profitable decision based on a report like this? 
+We’ve only identified which region is the most profitable, but let’s dig a little deeper and try to figure out why, step by step.
+What's next then ? Maybe it'd be nice to see performance over time.
+================================================================================================================================================================================================*/
+
+
+/*================================================================================================================================================================================================
+2️⃣ YoY performance
 ================================================================================================================================================================================================*/
 
 SELECT
@@ -62,13 +50,150 @@ WHERE o.delivery_state='California'
 GROUP BY YEAR,o.delivery_state
 ORDER BY year DESC
 
-/*================================================================================================================================================================================================
+/*================================================================================================================================================================================================	
+Query result snippet:
+
+| year | delivery_state | revenue    | orders_cnt |
+|------|----------------|------------|------------|
+| 2022 | California     |  16 186,48 |         37 |
+| 2021 | California     | 148 729,44 |        336 |
+| 2020 | California     | 121 925,07 |        279 |
+| 2019 | California     |  93 307,09 |        198 |
+| 2018 | California     |  71 302,47 |        171 |
+
+Result is suspicious... immediately raises a red flag.
+Between 2018 - 2021 California was doing fantastic and then in 2022... sudden ~90% revenue drop.
+Such a drastic change is highly unlikely from a business perspective.
+
+This query is a classic example of how misleading conclusions can arise from “just take the average” type of thinking.
+
 What am I going to do next:
-- validate data completeness and add months to the result
-- double check aggregation logic
+- validate data completeness and add months column to the result
+- double-check aggregation logic
 - adjust filtering to compare with other regions
+=================================================================================================================================================================================================*/
+
+/*=================================================================================================================================================================================================
+2️⃣.1️⃣ examinig a YoY red flag 
+=================================================================================================================================================================================================*/
+	
+SELECT
+	EXTRACT(YEAR FROM o.order_date)																					YEAR
+	,EXTRACT(MONTH FROM o.order_date)																				MONTH
+	,o.delivery_state
+	,ROUND(SUM(op.item_quantity*COALESCE(p.product_price,0)*(1-COALESCE(op.position_discount,0))), 2) 				revenue
+	,COUNT(DISTINCT op.order_id)																					orders_cnt
+FROM orders o
+JOIN order_positions op ON o.order_id = op.order_id
+JOIN products p ON op.product_id = p.product_id
+WHERE EXTRACT(YEAR FROM o.order_date) = 2022
+GROUP BY YEAR,MONTH,o.delivery_state
+ORDER BY YEAR DESC,MONTH DESC, revenue DESC;
+
+/*=================================================================================================================================================================================================
+Query result snippet:
+
+| year | month | delivery_state | revenue    | orders_cnt |
+|------|-------|----------------|------------|------------|
+| 2022 | 1     | California     | 16 186,48  |         37 |
+| 2022 | 1     | New York       |  5 757,49  |         19 |
+| 2022 | 1     | Kentucky       |  4 113,58  |          4 |
+| 2022 | 1     | Illinois       |  3 730,73  |         10 |
+| 2022 | 1     | Michigan       |  3 663,71  |          5 |
+
+As expected the data confirms that 2022 currently includes only January.
+This explains the apparent YoY revenue drop and indicates that the issue is related to data completeness rather than actual business performance.
+We can continue our work focusing on California.
 ================================================================================================================================================================================================*/
 
+/*===============================================================================================================================================================================================
+3️⃣ California's MoM performance - basic insight
+================================================================================================================================================================================================*/
+
+SELECT
+    EXTRACT(YEAR FROM o.order_date)                                                                         								year
+    ,EXTRACT(MONTH FROM o.order_date)                                                                       								month
+    ,o.delivery_state                                                                                        								delivery_state
+    ,ROUND(SUM(op.item_quantity * COALESCE(p.product_price,0) * (1 - COALESCE(op.position_discount,0))), 2) 								revenue
+    ,COUNT(op.order_id)                                                                                      								orders_cnt
+    ,COUNT(DISTINCT o.customer_id)                                                                          								unique_customers
+    ,ROUND(SUM(op.item_quantity * COALESCE(p.product_price,0) * (1 - COALESCE(op.position_discount,0))) /
+           COUNT(o.customer_id), 2)                                                                         								AoV
+FROM orders o
+JOIN order_positions op ON o.order_id = op.order_id
+JOIN products p ON op.product_id = p.product_id
+WHERE o.delivery_state = 'California'
+GROUP BY year, month, o.delivery_state
+ORDER BY year DESC,month DESC, revenue DESC;
+
+/*===============================================================================================================================================================================================	
+Query result snippet:
+
+| year | month | delivery_state |     revenue | orders_cnt | unique_customers |       AoV |
+|------|-------|----------------|------------|-------------|------------------|-----------|
+| 2022 |     1 | California     |  16 186,48 |          80 |               35 |    202,33 |
+| 2021 |    12 | California     |  13 860,23 |          86 |               49 |    161,17 |
+| 2021 |    11 | California     |  18 346,94 |          54 |               26 |    339,76 |
+| 2021 |    10 | California     |  15 769,12 |          83 |               40 |    189,99 |
+| 2021 |     9 | California     |  20 248,41 |          77 |               30 |    262,97 |
+
+We used month-over-month trends to confirm there is a complete data for every prior month. 
+It's a good moment to decide what we'd love to calculate and clarify the approach:
+	- including every variation of a metric can generate noise rather than insight
+	- the data must make logical sense, mixing every important metric into one table is definitely not what we want
+
+Next step: YoY metrics
+================================================================================================================================================================================================*/
+
+/*===============================================================================================================================================================================================
+4️⃣ YoY insight, defining important metrics, deleting redundant columns
+================================================================================================================================================================================================*/
+
+WITH base_metrics AS 
+(
+SELECT
+    EXTRACT(YEAR FROM o.order_date)                                                                         								year
+    ,EXTRACT(MONTH FROM o.order_date)                                                                       								month
+    ,o.delivery_state                                                                                        								delivery_state
+    ,ROUND(SUM(op.item_quantity * COALESCE(p.product_price,0) * (1 - COALESCE(op.position_discount,0))), 2) 								revenue
+    ,COUNT(op.order_id)                                                                                      								orders_cnt
+    ,COUNT(DISTINCT o.customer_id)                                                                          								unique_customers
+    ,ROUND(SUM(op.item_quantity * COALESCE(p.product_price,0) * (1 - COALESCE(op.position_discount,0))) /
+           COUNT(o.customer_id), 2)                                                                         								AoV
+FROM orders o
+JOIN order_positions op ON o.order_id = op.order_id
+JOIN products p ON op.product_id = p.product_id
+WHERE o.delivery_state = 'California'
+GROUP BY YEAR, MONTH, o.delivery_state
+ORDER BY YEAR DESC, MONTH DESC, REVENUE DESC
+)
+SELECT
+	delivery_state
+    ,year
+    ,month
+    ,revenue                                                                                               									current_year_revenue
+    ,LAG(revenue) OVER(PARTITION BY delivery_state, month ORDER BY year)                                   									last_year_revenue
+    ,orders_cnt
+    ,LAG(orders_cnt) OVER(PARTITION BY delivery_state, month ORDER BY year)                                   								last_year_orders_cnt
+    ,unique_customers
+    ,LAG(unique_customers) OVER(PARTITION BY delivery_state, month ORDER BY year)                                   						last_year_unique_customers
+    ,AoV
+    ,LAG(AoV) OVER(PARTITION BY delivery_state, month ORDER BY year)                                   										last_year_AoV
+FROM base_metrics
+ORDER BY year DESC, month DESC;
+
+/*================================================================================================================================================================================================
+Query result snippet:
+
+| delivery_state | year | month | current_year_revenue | last_year_revenue | orders_cnt | last_year_orders_cnt | unique_customers | last_year_unique_customers | AoV     | last_year_AoV |
+|----------------|------|-------|----------------------|-------------------|------------|----------------------|------------------|----------------------------|---------|---------------|
+| California     | 2022 |     1 |            16 186,48 |         19 957,45 |         80 |                   71 |               35 |                         33 |  202,33 |        281,09 |
+| California     | 2021 |    12 |            13 860,23 |         19 555,03 |         86 |                   82 |               49 |                         45 |  161,17 |        238,48 |
+| California     | 2021 |    11 |            18 346,94 |          8 693,27 |         54 |                   52 |               26 |                         26 |  339,76 |        167,18 |
+| California     | 2021 |    10 |            15 769,12 |         12 468,53 |         83 |                   65 |               40 |                         33 |  189,99 |        191,82 |
+| California     | 2021 |     9 |            20 248,41 |         11 782,73 |         77 |                   40 |               30 |                         19 |  262,97 |        294,57 |
+
+================================================================================================================================================================================================*/
 
 /*================================================================================================================================================================================================
 2️⃣YoY performance
